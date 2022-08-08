ffb106bee5e8f299aecf7475a531e8e3bd826ddd
Zakir Hussain <itsjakirhussain@gmail.com>
2022-08-08 22:56:03 +0530

    import fix

--- libs/database/src/index.ts
+++ libs/database/src/index.ts
@@ -1,11 +1,11 @@
 export * from './lib/database';
 
-export * from './lib/models/accounting';
 export * from './lib/models/attendance';
-export * from './lib/models/availability-setting';
-export * from './lib/models/product';
-export * from './lib/models/order';
-export * from './lib/models/consumer';
 export * from './lib/models/bill';
-export * from './lib/models/shop';
+export * from './lib/models/consumer';
 export * from './lib/models/employee';
+export * from './lib/models/order';
+export * from './lib/models/product-setting';
+export * from './lib/models/product';
+export * from './lib/models/shop';
+export * from './lib/models/transaction';
